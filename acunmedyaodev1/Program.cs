36d06272b575416fa1e2593cfff14e5c1d06ad85
AliKiziltas <alikiziltas477@gmail.com>
2025-02-24 14:51:15 +0300

    Acun Medya Akademi 2. hafta ödev

---
+++ acunmedyaodev1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using acunmedyaodev1.classes;
+
+Console.WriteLine("Hello, World!");
+Bmw bm = new Bmw();
+bm.marka = "Bmw";
+bm.vites = "Düz";
+bm.vites_durumu(bm.marka, bm.vites);
+
+Porsche ph = new Porsche();
+ph.marka = "Porsche";
+ph.vites = "Otomatik";
+ph.vites_durumu(ph.marka, ph.vites);
+
+Togg tg = new Togg();
+tg.marka = "Togg";
+tg.vites = "Otamatik";
+tg.vites_durumu(tg.marka, tg.vites);
+
+Mercedes mr = new Mercedes();
+mr.marka = "Mercedes";
+mr.vites = "Düz";
+mr.vites_durumu(mr.marka, mr.vites);
+
+Toyota tyt = new Toyota();
+tyt.marka = "Toyota";
+tyt.vites = "Düz";
+tyt.vites_durumu(tyt.marka, tyt.vites);
+
+Audi au = new Audi();
+au.marka = "Audi";
+au.vites = "Otomatik";
+au.vites_durumu(au.marka, au.vites);
---
+++ acunmedyaodev1/classes/Araba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acunmedyaodev1.classes
+{
+    public class Araba
+    {
+        public string marka = null;
+        public string model = null;
+        public string vites = null;
+        public int kapisayisi = 0;
+        public int penceresayisi = 0;
+        public void vites_durumu(string markasi, string vitesi)
+        {
+            Console.WriteLine(markasi + " " + vitesi + " viteslidir.");
+        }
+    }
+}
