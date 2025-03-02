dea3df485002d788e362945bf9c67f03cf267b97
AliKiziltas <alikiziltas477@gmail.com>
2025-03-02 21:12:20 +0300

    AcunMedya

--- Odev2/acunmedyaodev2/obj/Debug/net8.0/acunmedyaodev2.AssemblyInfo.cs
+++ Odev2/acunmedyaodev2/obj/Debug/net8.0/acunmedyaodev2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acunmedyaodev2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414bdc32cceb48d49884ce70a77ae40defc8c9a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("acunmedyaodev2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acunmedyaodev2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
