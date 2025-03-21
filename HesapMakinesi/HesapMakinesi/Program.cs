f57bc0945ab304f7e01d6e6635c07678ed865eb2
AliKiziltas <alikiziltas477@gmail.com>
2025-03-21 18:33:51 +0300

    Acunmedya

---
+++ HesapMakinesi/HesapMakinesi/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sayi1, sayi2;
+        string islem;
+
+        Console.Write("1. sayıyı giriniz: ");
+        while (!int.TryParse(Console.ReadLine(), out sayi1))
+        {
+            Console.Write("Geçersiz giriş! Lütfen bir sayı giriniz: ");
+        }
+
+        
+        Console.Write("Yapacağınız işlemi belirtin (+, -, *, /): ");
+        while (true)
+        {
+            islem = Console.ReadLine();
+            if (islem == "+" || islem == "-" || islem == "*" || islem == "/")
+            {
+                break; 
+            }
+            Console.Write("Geçersiz işlem! Lütfen sadece +, -, *, / giriniz: ");
+        }
+        Console.Write("2. sayıyı giriniz: ");
+        while (!int.TryParse(Console.ReadLine(), out sayi2))
+        {
+            Console.Write("Geçersiz giriş! Lütfen bir sayı giriniz: ");
+        }
+
+        
+        while (islem == "/" && sayi2 == 0)
+        {
+            Console.WriteLine("Bölen sayı 0 olamaz.");
+            Console.Write("Lütfen geçerli bir sayı giriniz: ");
+            while (!int.TryParse(Console.ReadLine(), out sayi2))
+            {
+                Console.Write("Geçersiz giriş! Lütfen bir sayı giriniz: ");
+            }
+        }
+
+        
+        if (islem == "*")
+        {
+            Console.WriteLine($"Sonuç: {sayi1 * sayi2}");
+        }
+        else if (islem == "-")
+        {
+            Console.WriteLine($"Sonuç: {sayi1 - sayi2}");
+        }
+        else if (islem == "+")
+        {
+            Console.WriteLine($"Sonuç: {sayi1 + sayi2}");
+        }
+        else if (islem == "/")
+        {
+            Console.WriteLine($"Sonuç: {sayi1 / (double)sayi2}");
+        }
+    }
+}
