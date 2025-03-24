27dc16ce97c3224d38cb8b2946af91f31dd9f62d
AliKiziltas <alikiziltas477@gmail.com>
2025-03-24 17:03:42 +0300

    Acunmedya

---
+++ acunmedyaodev5/acunmedyaodev5/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<Car> cars = new List<Car>
+        {
+            new Car("Toyota", "Corolla", 6.5, 5000),
+            new Car("BMW", "X5", 9.0, 5000),
+            new Car("Mercedes", "C200", 8.4, 5000)
+        };
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine("Marka: " + car.Brand + "Model: " + car.Model + " 100 km'de Yaktığı Yakıt: " + car.FuelConsumption + " litre"+
+     "Toplam Yakıt Tüketimi: " + car.CalculateTotalFuelConsumption() + " litre");
+        }
+    }
+}
---
+++ acunmedyaodev5/acunmedyaodev5/classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Car
+{
+    public string Brand;
+    public string Model;
+    public double FuelConsumption; // 100 km başına harcanan yakıt (litre)
+    public double TotalDistance;   // Toplam mesafe (km)
+
+    public Car(string brand, string model, double fuelConsumption, double totalDistance)
+    {
+        Brand = brand;
+        Model = model;
+        FuelConsumption = fuelConsumption;
+        TotalDistance = totalDistance;
+    }
+
+    public double CalculateTotalFuelConsumption()
+    {
+        return (TotalDistance / 100) * FuelConsumption;
+    }
+}
